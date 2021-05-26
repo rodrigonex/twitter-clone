34f90d777bc7af94cb9e0816f69eb22859ddf820
Your Name <rodrigoe923@gmail.com>
2021-05-26 19:09:55 -0300

    first commit

---
+++ get_tweet.php
@@ -0,0 +1,36 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        
+        header('Location: index.php');
+
+    }
+
+    require_once('db.class.php');
+
+    $id_usuario = $_SESSION['id_usuario'];
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    $sql = " SELECT DATE_FORMAT(t.data_inclusao, '%d %b %Y %T') AS padrao_data_inclusao, t.tweet, u.usuario"; 
+    $sql.= " FROM tweet AS t JOIN usuarios AS u ON (t.id_usuario = u.id)";
+    $sql.= " WHERE id_usuario = $id_usuario ORDER BY data_inclusao DESC";
+    
+    $resultado_id = mysqli_query($link, $sql);
+  
+    if($resultado_id){
+        while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
+
+            echo '<a href="#" class="list-group-item"';
+                echo '<h4 class="list-group-item-heading">'.$registro['usuario'].' - <small>'.$registro['padrao_data_inclusao'].'</small></h4>';
+                echo '<p class="list-group-item-text">'.$registro['tweet'].'</p>';
+            echo '</a>';
+        }
+    }else{
+        echo 'Erro na consulta do tweets no banco de dados!';
+    }
+
+?>
---
+++ registra_usuario.php
@@ -0,0 +1,71 @@
+<?php
+
+    require_once('db.class.php');
+
+
+    $login = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = md5($_POST['senha']);
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    $usuario_existe = false;
+    $email_existe = false;
+
+    //verificar se o usuario já existe
+    $sql = " select * from usuarios where usuario = '$login' ";
+
+    if( $resultado_id = mysqli_query($link, $sql)) {
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['usuario'])){
+            $usuario_existe = true;
+        }
+    }else{
+        echo 'Erro ao tentar localizar o registro do usuario';
+    }
+
+    //verificar se a senha já existe
+    
+    $sql = " select * from usuarios where email = '$email' ";
+
+    if( $resultado_id = mysqli_query($link, $sql)) {
+
+        $dados_email = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_email['email'])){
+            $email_existe = true;
+        }
+    }else{
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+   if($usuario_existe || $email_existe){
+
+        $retorno_get = '';
+
+        if($usuario_existe){
+            $retorno_get.= "erro_usuario=1&";     
+        }
+
+        if($email_existe){
+            echo'dentro do if';
+            $retorno_get.= "erro_email=1&";  
+        }
+        
+        header('Location: inscrevase.php?'.$retorno_get);
+        die();
+    }
+
+    $sql = " insert into usuarios(usuario, email, senha) values ('$login', '$email', '$senha') ";
+
+    //exercutar a query
+    if(mysqli_query($link, $sql)){
+        echo'Usuário registrado com sucesso!';
+    }else{
+        echo 'Erro ao registrar usuário!';
+    }
+    
+?>
