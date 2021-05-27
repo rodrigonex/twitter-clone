316155215387e10ae7795e59536ce365bf44d685
Your Name <rodrigoe923@gmail.com>
2021-05-27 18:16:32 -0300

    Atualização da tela de login

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 
 		<!-- bootstrap - link cdn -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="./style/style.css">
 	
 		<script>
 			// código javascript	
@@ -50,65 +51,37 @@
 	<body>
 
 		<!-- Static navbar -->
-	    <nav class="navbar navbar-default navbar-static-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <img src="imagens/icone_twitter.png" />
-	        </div>
-	        
-	        <div id="navbar" class="navbar-collapse collapse">
-	          <ul class="nav navbar-nav navbar-right">
-	            <li><a href="inscrevase.php">Inscrever-se</a></li>
-	            <li class="<?= $erro == 1 ? 'open' : '' ?>">
-	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
-					<ul class="dropdown-menu" aria-labelledby="entrar">
-						<div class="col-md-12">
-				    		<p>Você possui uma conta?</h3>
-				    		<br />
-							<form method="post" action="validar_acesso.php" id="formLogin">
-								<div class="form-group">
-									<input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Usuário" />
-								</div>
-								
-								<div class="form-group">
-									<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
-								</div>
-								
-								<button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
-			
-								<br /><br />
-								<?php
-									if($erro == 1) echo '<font color-"#ff0000" Usuário e ou senha inválido(s).</font>';
-								?>
-							</form>
-
-						</form>
-				  	</ul>
-	            </li>
-	          </ul>
-	        </div><!--/.nav-collapse -->
-	      </div>
+	    <nav class="logo">               
+			<span><img src="./imagens/icone_twitter.png" /> </span>
 	    </nav>
 
 
-	    <div class="container">
+	    <div class="containerflex">
+			<form method="post" action="validar_acesso.php" id="formLogin">
+				<h1>Entrar no Twitter</h1>
+				<div class="form-group">
+					<input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Nome de Usuário" />
+				</div>
+								
+				<div class="form-group">
+					<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
+				</div>
+								
+				<button type="buttom" class="btn btn-primary" id="btn_login" style="width: 100%;">Entrar</button>
 
+		
+				<div class="outros">
+					<a href="#">Esqueceu sua senha?</a> -
+					<a href="inscrevase.php">Inscrever-se no Twitter</a>
+				</div>
+				<?php
+				if($erro == 1) echo '<font color-"#ff0000" Usuário e ou senha inválido(s).</font>';
+				?>
+			</form>
 	      <!-- Main component for a primary marketing message or call to action -->
-	      <div class="jumbotron">
-	        <h1>Bem vindo ao twitter clone</h1>
-	        <p>Veja o que está acontecendo agora...</p>
-	      </div>
-
-	      <div class="clearfix"></div>
 		</div>
 
-
+		</div>
 	    </div>
 	
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
